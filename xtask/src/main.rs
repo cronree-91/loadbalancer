332b119c0c6671b5fef5736477ed4e5798811c70
Unixtech-06 <ryosuke_666@icloud.com>
2023-12-07 11:41:24 +0900

    add comment xtask/src/main.rs

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -2,15 +2,17 @@ mod build_ebpf;
 mod run;
 
 use std::process::exit;
-
 use clap::Parser;
 
+// Command line options struct
 #[derive(Debug, Parser)]
 pub struct Options {
+    // Using clap's subcommand functionality to handle different commands
     #[clap(subcommand)]
     command: Command,
 }
 
+// Enum for different commands
 #[derive(Debug, Parser)]
 enum Command {
     BuildEbpf(build_ebpf::Options),
@@ -18,16 +20,18 @@ enum Command {
 }
 
 fn main() {
-    let opts = Options::parse();
+    let opts = Options::parse(); // Parsing command line arguments
 
-    use Command::*;
+    // Match the provided command and execute the corresponding function
     let ret = match opts.command {
-        BuildEbpf(opts) => build_ebpf::build_ebpf(opts),
-        Run(opts) => run::run(opts),
+        Command::BuildEbpf(opts) => build_ebpf::build_ebpf(opts),
+        Command::Run(opts) => run::run(opts),
     };
 
+    // Error handling: prints error and exits if an error occurs
     if let Err(e) = ret {
-        eprintln!("{e:#}");
+        eprintln!("Error: {e:#}");
         exit(1);
     }
 }
+
